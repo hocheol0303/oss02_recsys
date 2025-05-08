c64c2cf403b36db6c74055f81234a176f9272e6b
hobbang <hocheol0303@gmail.com>
2025-05-09 08:17:49 +0900

    inference 결과 json으로 저장

--- model03_id_NCF/inference03_uid_ilist.py
+++ model03_id_NCF/inference03_uid_ilist.py
@@ -1,10 +1,11 @@
 import torch
 import pandas as pd
 import numpy as np
+import json
 from model03_id_NCF.model03 import NCF
 
 MODEL_PATH = "/Users/myserver/workspace/oss/model03_id_NCF/saved_models/movie_64_50Epoch_64Batch_LR0.001_1000Users_500Items/epoch19_20250507_164830_valrmse0.8963.pt"
-OUTPUT_PATH = "/Users/myserver/workspace/oss/tmp/inference_result.csv"
+OUTPUT_PATH = "/Users/myserver/workspace/oss/tmp/inference_result.json"
 
 USER_ID = 1
 ITEM_IDS = [1, 2, 3, 4, 5]
@@ -16,7 +17,7 @@ def load_model(model_state_dict, num_users, num_items):
     model.eval()
     return model
 
-def predict(model, user_id:int, item_ids:list, output_path="inference_result.csv", batch_size=64):
+def predict(model, user_id:int, item_ids:list, output_path="inference_result.json"):
     device = torch.device("cuda" if torch.cuda.is_available() 
                           else "mps" if torch.backends.mps.is_available() 
                           else "cpu")
@@ -31,10 +32,18 @@ def predict(model, user_id:int, item_ids:list, output_path="inference_result.csv
     with torch.no_grad():
         preds = model(user_tensor, item_tensor)
 
-    results = list(zip([user_id]*len(item_ids), item_ids, preds.cpu().numpy()))
-    print(f"User {user_id}의 예측 결과:")
-    for user_id, item_id, pred in results:
-        print(f"userId: {user_id}, itemId: {item_id}, predicted_rating: {pred:.4f}")
+    results = []
+    for item_id, pred in zip(item_ids, preds):
+        results.append({
+            "userId": user_id,
+            "itemId": item_id,
+            "predicted_rating": pred.item()
+        })
+    
+    with open(output_path, "w") as f:
+        json.dump(results, f, indent=2)
+    
+    print(f"예측 결과 저장 완료: {output_path}")
 
 
 if __name__ == "__main__":
