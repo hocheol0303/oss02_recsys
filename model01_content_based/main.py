0236e4b1b20861f0045514554bffa37d0408f624
yunaaaaoh <yuno00@dgu.ac.kr>
2025-05-14 10:24:05 +0900

    Update main.py

--- model01_content_based/main.py
+++ model01_content_based/main.py
@@ -9,11 +9,11 @@ app = FastAPI()
 # PostgreSQL 연결 함수
 def get_connection():
     return psycopg2.connect(
-        host="localhost",       # 또는 '127.0.0.1'
-        dbname="infotree",  # pgAdmin에서 사용하는 DB 이름
-        user="postgres",   # PostgreSQL 사용자 (보통 postgres)
-        password="oyun1211",  # 해당 사용자의 비밀번호
-        port=5433               # PostgreSQL 기본 포트
+        host="localhost",
+        dbname="infotree",
+        user="infotree", 
+        password="info1234",
+        port=5433
     )
 
 @app.get("/recommendations/{user_id}")
@@ -21,7 +21,7 @@ def recommend(user_id: int):
     conn = get_connection()
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
-    # 사용자 정보 조회
+    # users
     cur.execute("SELECT categories FROM users WHERE id = %s", (user_id,))
     user = cur.fetchone()
     if not user:
@@ -29,7 +29,7 @@ def recommend(user_id: int):
         conn.close()
         return {"error": "User not found"}
 
-    # 혜택 정보 조회
+    # benefits
     cur.execute("SELECT id, categories FROM benefits")
     benefits = cur.fetchall()
     print("benefits:", benefits)
@@ -37,7 +37,6 @@ def recommend(user_id: int):
     cur.close()
     conn.close()
 
-    # 추천 실행
     top_benefits = get_recommendations(user, benefits)
 
     return {
